cb5b8b16258d6b5e8e18e388db3d264639793eca
prianti29 <59369461+prianti29@users.noreply.github.com>
2023-03-09 22:32:54 +0600

    final

--- main.js
+++ main.js
@@ -1,28 +1,27 @@
 (function () {
-    let screen = document.querySelector('.screen');
+    let screen1 = document.querySelector('.screen');
     let buttons = document.querySelectorAll('.btn');
     let clear = document.querySelector('.btn-clear');
     let equal = document.querySelector('.btn-equal');
 
     //to check each and every button in working
     buttons.forEach(function (button) {
-
         button.addEventListener('click', function (e) {
             let value = e.target.dataset.num; //take data when click the button
-            screen.value += value; // push the value into screen
+            // console.log(value);
+            screen1.value += value; // push the value into screen
         })
     });
-    equal.addEventListener('click', function (e) {
-        if (screen.value === '') {
-            screen.value = "";
+    equal.addEventListener('click', function (e){
+        if (screen1.value === '') {
+            screen1.value = "";
         } else {
-            let answer = eval(screen.value);
-            // screen.value = answer;
-            console.log(answer)
+            // console.log(screen.value);
+            let answer = eval(screen1.value);
+            screen1.value = answer;
         }
     });
-
     clear.addEventListener('click', function (e) {
-        screen.value = "";
+        screen1.value = "";
     });
 })();
